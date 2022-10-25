bbc944d665427131d79744a434972ede6449029a
David <david>
2022-10-25 20:26:55 -0300

    Add Teste de unidade

---
+++ src/Projeto.Application.Tests.Unit/UseCases/WeatherForecastUseCaseTest.cs
@@ -0,0 +1,21 @@
+﻿using Projeto.Application.UseCases.Modelo;
+using Projeto.Application.UseCases.WeatherForecast;
+using Xunit;
+
+namespace Projeto.Application.Tests.Unit.UseCases;
+
+public sealed class WeatherForecastUseCaseTest
+{
+    [Fact]
+    public async Task Execute_SeExecutadoComSucesso_EntaoRetornaResponse()
+    {
+        //Arrange
+        var useCase = new WeatherForecastUseCase();
+
+        //Act
+        var response = await useCase.Execute(new WeatherForecastRequest());
+
+        //Assert
+        Assert.NotEmpty(response.Content);
+    }
+}
