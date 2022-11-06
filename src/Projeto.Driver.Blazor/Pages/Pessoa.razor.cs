0439401209614ead33260099bf9a48b6303af7f1
David Oliveira <david.oliveira@dmcard.com.br>
2022-11-06 01:24:29 -0300

    Add sweetalert2

--- src/Projeto.Driver.Blazor/Pages/Pessoa.razor.cs
+++ src/Projeto.Driver.Blazor/Pages/Pessoa.razor.cs
@@ -7,42 +7,83 @@ namespace Projeto.Driver.Blazor.Pages;
 
 public partial class Pessoa
 {
-    private static IJSRuntime? _jsRuntime;
-    private List<PessoaModel>? _listaPessoas;
+    private static Pessoa _instance = null!;
+    private static IJSRuntime _jsRuntime = null!;
+    private static HttpClient _http = null!;
+    private static List<PessoaModel>? _listaPessoas;
     private PessoaModel _pessoaSelecionada = new();
-
+    private string _tituloModal = string.Empty;
+    
     protected override async Task OnInitializedAsync()
     {
-        _listaPessoas = await Http.GetFromJsonAsync<List<PessoaModel>>("https://localhost:44358/Pessoa/Listar");
+        _instance = this;
+        _http = Http;
+        _listaPessoas = (await Http.GetFromJsonAsync<List<PessoaModel>>("https://localhost:44358/Pessoa/Listar"))!;
         _jsRuntime = JsRuntime;
+    }
+
+    private async Task IncluirOnClick()
+    {
+        _tituloModal = "Incluir Pessoa";
+        _pessoaSelecionada = new();
+        await JsRuntime.InvokeVoidAsync("MethodCstoJsCall", "Incluir", _pessoaSelecionada.CodigoUniversal);
     }
 
     private async Task EditarOnClick(PessoaModel pessoa)
     {
+        _tituloModal = "Alterar Pessoa";
         _pessoaSelecionada = pessoa.Clone<PessoaModel>()!;
         await JsRuntime.InvokeVoidAsync("MethodCstoJsCall", "Editar", pessoa.CodigoUniversal);
     }
 
     private async Task ExcluirOnClick(PessoaModel pessoa)
     {
-        using var response = await Http.DeleteAsync($"https://localhost:44358/Pessoa/Excluir/?codigo={pessoa.CodigoUniversal.GetValueOrDefault():D}");
-        _listaPessoas?.Remove(pessoa);
         await JsRuntime.InvokeVoidAsync("MethodCstoJsCall", "Excluir", pessoa.CodigoUniversal);
+        await JsRuntime.InvokeVoidAsync("messageConfirma", pessoa);
+    }
+
+    private Task AddOrUpdadeList()
+    {
+        var index = _listaPessoas!.FindIndex(model => model.CodigoUniversal.Equals(_pessoaSelecionada.CodigoUniversal));
+        if (index == -1)
+        {
+            _listaPessoas.Add(_pessoaSelecionada);
+            return Task.CompletedTask;
+        }
+
+        _listaPessoas[index] = _pessoaSelecionada;
+        return Task.CompletedTask;
     }
 
     private async Task SaveOnClick()
     {
         using var response = await Http.PostAsJsonAsync("https://localhost:44358/Pessoa/Gravar", _pessoaSelecionada);
+
         _pessoaSelecionada = (await response.Content.ReadFromJsonAsync<PessoaModel>())!;
-        _listaPessoas![_listaPessoas.FindIndex(model => model.CodigoUniversal.GetValueOrDefault().Equals(_pessoaSelecionada.CodigoUniversal.GetValueOrDefault()))] = _pessoaSelecionada;
+        await AddOrUpdadeList();
         await JsRuntime.InvokeVoidAsync("closeModal");
     }
 
     [JSInvokable]
-    public static Task MethodJstoCsCall(string nameEvent, string codigo)
+    public static async Task MethodJstoCsCall(string nameEvent, string codigo)
     {
         var mensagem = $"Executou evento {nameEvent}, codigo {codigo}";
-        _jsRuntime?.InvokeVoidAsync("exibeMensagem", mensagem).GetAwaiter();
-        return Task.CompletedTask;
+        await _jsRuntime.InvokeVoidAsync("exibeMensagem", mensagem);
+    }
+
+    [JSInvokable]
+    public static async Task ExclusaoConfirmada(PessoaModel pessoa)
+    {
+        using var response = await _http.DeleteAsync($"https://localhost:44358/Pessoa/Excluir/?codigo={pessoa.CodigoUniversal:D}");
+        
+        var pessoaSelecionada = _listaPessoas!.FirstOrDefault(model => model.CodigoUniversal.Equals(pessoa.CodigoUniversal))!;
+
+        var mensagem = "NÃO achou na lista!!!";
+        if (_listaPessoas!.Remove(pessoaSelecionada))
+            mensagem = "achou na lista!!!";
+        await _jsRuntime.InvokeVoidAsync("exibeMensagem", mensagem);
+
+        await _jsRuntime.InvokeVoidAsync("exibeMensagem", "exclusão!!!");
+        _instance.StateHasChanged();
     }
 }
--- src/Projeto.Driver.Blazor/wwwroot/js/blazor.pages.pessoa.js
+++ src/Projeto.Driver.Blazor/wwwroot/js/blazor.pages.pessoa.js
@@ -9,4 +9,30 @@ exibeMensagem = (mensagem) => {
 
 closeModal = () => {
     $('#exampleModal').modal('hide')
+    showMessage();
+}
+
+messageConfirma = (pessoa) => {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        //console.log(pessoa)
+        //console.log(result)
+        //return result.isConfirmed        
+        if (result.isConfirmed) {
+            console.log(pessoa);
+            DotNet.invokeMethodAsync('Projeto.Driver.Blazor', 'ExclusaoConfirmada', pessoa);
+            //Swal.fire(
+            //    'Deleted!',
+            //    'Your file has been deleted.',
+            //    'success'
+            //)
+        }
+    })
 }
